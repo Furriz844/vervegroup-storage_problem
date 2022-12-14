510327f2e71c1ea1ae74dfba010bfbb46e11a332
vasilyevd844 <vasilyevd844@yandex.ru>
2022-12-14 10:25:05 +0300

    use db for store promotions

--- cmd/storage-api/main.go
+++ cmd/storage-api/main.go
@@ -6,15 +6,64 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"storage-api/internal/app/store"
+
+	"fmt"
+
+	"database/sql"
+
+	_ "github.com/lib/pq"
+
+	"log"
 )
 
+var DB *sql.DB
+var repo store.PromotionRepository
+
+// todo to settings file
+const (
+	HOST     = "localhost"
+	PORT     = 55000
+	USER     = "postgres"
+	PASSWORD = "postgrespw"
+	DBNAME   = "storage_api"
+)
+
+func main() {
+	connString := fmt.Sprintf(
+		"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+		HOST, PORT, USER, PASSWORD, DBNAME,
+	)
+
+	DB, err := sql.Open("postgres", connString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer DB.Close()
+	repo = store.NewRepository(DB)
+
+	router := gin.Default()
+	router.GET("/promotions", getPromotions)
+	router.GET("/promotions/:id", GetPromotionById)
+	router.GET("/admin/load", LoadPromotions)
+
+	router.Run("localhost:8080")
+}
+
+func NewRepository(DB *sql.DB) {
+	panic("unimplemented")
+}
+
 func getPromotions(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, store.GetPromotions())
+	c.IndentedJSON(http.StatusOK, repo.GetPromotions())
 }
 
 func GetPromotionById(c *gin.Context) {
 	id := c.Param("id")
-	promotion := store.GetPromotionById(id)
+	promotion, err := repo.GetPromotionById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": err})
+		return
+	}
 	if promotion == nil {
 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "promotion not found"})
 		return
@@ -22,15 +71,6 @@ func GetPromotionById(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, promotion)
 }
 func LoadPromotions(c *gin.Context) {
-	store.LoadFromCsv()
+	repo.LoadFromCsv()
 	c.IndentedJSON(http.StatusOK, gin.H{"status": "ok"})
-}
-
-func main() {
-	router := gin.Default()
-	router.GET("/promotions", getPromotions)
-	router.GET("/promotions/:id", GetPromotionById)
-	router.GET("/admin/load", LoadPromotions)
-
-	router.Run("localhost:8080")
 }
--- internal/app/store/promotionrepository.go
+++ internal/app/store/promotionrepository.go
@@ -1,6 +1,7 @@
 package store
 
 import (
+	"database/sql"
 	"encoding/csv"
 	"log"
 	"os"
@@ -8,23 +9,39 @@ import (
 	"strconv"
 )
 
+type PromotionRepository struct {
+	db *sql.DB
+}
+
+func NewRepository(database *sql.DB) PromotionRepository {
+	return PromotionRepository{database}
+}
+
 var promotions = []*model.Promotion{}
 
-func GetPromotions() []*model.Promotion {
+func (r PromotionRepository) GetPromotions() []*model.Promotion {
 	return promotions
 }
 
-func GetPromotionById(id string) *model.Promotion {
-	for _, a := range promotions {
-		if a.ID == id {
-			return a
+func (r PromotionRepository) GetPromotionById(id string) (*model.Promotion, error) {
+	rows, err := r.db.Query("select * from promotion where id = $1", id)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	defer rows.Close()
+	p := model.Promotion{}
+	for rows.Next() {
+		if err := rows.Scan(&p.ID, &p.Price, &p.ExpirationDate); err != nil {
+			log.Fatal(err)
+			return nil, err
 		}
 	}
-	return nil
+	return &p, nil
 }
 
-func LoadFromCsv() {
-	csvFile := "./promotions.csv"
+func (r PromotionRepository) LoadFromCsv() {
+	csvFile := "./resources/promotions.csv"
 	f, err := os.Open(csvFile)
 	if err != nil {
 		log.Fatal("Unable to read input file "+csvFile, err)
@@ -36,12 +53,23 @@ func LoadFromCsv() {
 	if err != nil {
 		log.Fatal("Unable to parse file as CSV for "+csvFile, err)
 	}
+	tx, err := r.db.Begin()
 	for _, record := range records {
 		//todo check 3 element in array
 		price, err := strconv.ParseFloat(record[1], 64)
 		if err != nil {
 			log.Fatal("Can't parse record with id=", record[0], "Can't parse Price:", err)
 		}
-		promotions = append(promotions, &model.Promotion{ID: record[0], Price: price, ExpirationDate: record[2]})
+		//promotions = append(promotions, &model.Promotion{ID: record[0], Price: price, ExpirationDate: record[2]})
+		res, err := r.db.Exec("INSERT INTO promotion (id, price, date) VALUES ($1, $2, $3)", record[0], price, record[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+		rowCnt, err := res.RowsAffected()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("affected = %d\n", rowCnt)
 	}
+	tx.Commit()
 }
